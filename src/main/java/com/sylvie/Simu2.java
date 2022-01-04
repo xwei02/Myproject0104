ffe0851c3c499d47d00abae2a66c3f00bd0c963a
USER <sylvie.wu@hotmail.com>
2022-01-04 15:10:46 +0800

    commit

---
+++ src/main/java/com/sylvie/Simu2.java
@@ -0,0 +1,14 @@
+package com.sylvie;
+
+import java.util.Scanner;
+
+public class Simu2 {
+    public static void main(String[] args) {
+        int n = -1;
+        Scanner scanner = new Scanner(System.in);
+        while (n != 0){
+            System.out.println("buy item:");
+
+        }
+    }
+}
